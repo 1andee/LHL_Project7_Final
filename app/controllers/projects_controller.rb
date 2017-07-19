0be6849879fe4b6d759ba2bdf33aa986e3963313
john-lennie <johnanthonylennie@gmail.com>
2017-07-19 21:50:14 +0000

    in the middle of setting up new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,15 +6,4 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
   end
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:avatar, :name, :email, :password, :password_confirmation, :location, :linkedin_url, :github_url, :other_site, :twitter, :slack_name)
-  end
 end
