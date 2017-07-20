8df4fda36803e98727373cffa6568dd5b6c55170
Chasteau <davidchasteau@gmail.com>
2017-07-20 05:56:16 +0000

    found solution for partial projects problem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
   def index
     # @test = Skill.all
     # @users = User.search_by_name(params[:query])
-    @users = User.search_by_skill(params[:query])
-    @users_id = User.search_by_skill(params[:query]).pluck(:id)
+    # @users = User.search_by_skill(params[:query])
+    # @users_id = User.search_by_skill(params[:query]).pluck(:id)
 
-    @mentor_skills_id = SkillUser.where(user_id: @users_id, mentor: true).pluck(:skill_id)
-    @mentor_skills = Skill.where(id: @mentor_skills_id)
+    # @mentor_skills_id = SkillUser.where(user_id: @users_id, mentor: true).pluck(:skill_id)
+    # @mentor_skills = Skill.where(id: @mentor_skills_id)
+     # @skills = Skill.joins(:users, :skill_users).where(skill_users: {mentor: true, user_id: 13})
+    @users = User.search_by_skill(params[:query]).includes([:skills, :skill_users])
   end
 end
