11cf7689d1c949b0e70278b4fbcafe1dda8893ab
Chasteau <davidchasteau@gmail.com>
2017-07-20 02:12:35 +0000

    so close

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
+    # @test = Skill.all
     # @users = User.search_by_name(params[:query])
     @users = User.search_by_skill(params[:query])
+    @users_id = User.search_by_skill(params[:query]).pluck(:id)
+
+    @mentor_skills_id = SkillUser.where(user_id: @users_id, mentor: true).pluck(:skill_id)
+    @mentor_skills = Skill.where(id: @mentor_skills_id)
   end
 end
