737a56ba9fa0c51c41c9b2dca1dec5bb3c1577b3
Ian Duke <ian.c.duke@gmail.com>
2017-07-21 21:59:40 +0000

    Archive view restricts to current user

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,2 +1,16 @@
 class ArchiveController < ApplicationController
+
+  before_action :user_lookup
+
+  private
+
+  def user_lookup
+    @user = User.find(params[:user_id])
+  end
+
+  def index
+    @project_users = User.includes([:skills, :skill_users, :mentee_projects, :mentor_projects]).where(users: {id: @user.id})
+  end
+
+
 end
