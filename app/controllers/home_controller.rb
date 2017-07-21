7ebbe119806d57475c613d897c28e55dc5848027
john-lennie <johnanthonylennie@gmail.com>
2017-07-21 19:25:47 +0000

    made changes to projects sidebar to show ony users projects and sort by either mentor or mentee project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
     # @users = User.search_by_name(params[:query])
     # @users = User.search_by_skill(params[:query])
-
+    @projects = Project.all
+    @project = Project.find_by_id(params[:id])
     # data for view to display partial profile in search
     @users = User.search_by_skill(params[:query]).includes([:skills, :skill_users, :mentee_projects, :mentor_projects])
     # data for view to display partial projects on homepage
