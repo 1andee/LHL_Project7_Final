45cbc503105b9d45395e13c7b35132b5a7f6c04b
Ian Duke <ian.c.duke@gmail.com>
2017-07-22 01:39:44 +0000

    Archive is filtering by completion status, showing completed/cancelled projects only

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -7,8 +7,22 @@ class ArchiveController < ApplicationController
   end
 
   def index
-    @project_users = User.includes([:skills, :skill_users, :mentee_projects, :mentor_projects]).where(users: {id: @user.id})
-  end
+    @project_users = User.includes([:skills, :skill_users, :mentee_projects, :mentor_projects]).where(users: {id: @user.id}, projects: {:completion_status_id => [2, 3]} )
 
+    # @project_completion_statuses = Project
+    #                 .joins("INNER JOIN completion_statuses ON projects.completion_status_id = completion_statuses.id")
+    #                 .select("projects.*, completion_statuses.name")
+    #                 .where(project_id: @project.id)
+    #                 .order(id: :desc)
+    #
+    # @project_goals = Goal
+    #                 .joins("INNER JOIN completion_statuses ON goals.completion_status_id = completion_statuses.id")
+    #                 .select("goals.*, completion_statuses.name")
+    #                 .where(project_id: @project.id)
+    #                 .order(id: :desc)
+
+
+
+  end
 
 end
